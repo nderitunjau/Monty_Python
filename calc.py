a3356a7c73b25ce6444ec8fe7a021710dc79dada
nderitu_njau <nderitunjau@gmail.com>
2023-03-06 16:29:35 +0300

    Update calc.py
    Simple Calculate to add two numbers.

--- calc.py
+++ calc.py
@@ -13,6 +13,7 @@ Type "help", "copyright", "credits" or "license" for more information.
 >>> first_number = int(input('Type the first number: ')) ;\
 ... second_number = int(input('Type the second number: ')) ;\
 ... print("The sum is: ", first_number + second_number)
+
 Type the first number: 2
 Type the second number: 4
 The sum is:  6
