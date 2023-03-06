e90c6e1ad232ec85bc52b306d960a99e15cb1faa
nderitu_njau <nderitunjau@gmail.com>
2023-03-06 16:29:07 +0300

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,6 +1,5 @@
 //Azure sandbox option
 Connecting terminal...
-
 Welcome to Azure Cloud Shell
 
 Type "az" to use Azure CLI
@@ -10,6 +9,7 @@ nderitunjau [ ~ ]$ python
 Python 3.9.14 (main, Dec 15 2022, 01:12:13) 
 [GCC 11.2.0] on linux
 Type "help", "copyright", "credits" or "license" for more information.
+
 >>> first_number = int(input('Type the first number: ')) ;\
 ... second_number = int(input('Type the second number: ')) ;\
 ... print("The sum is: ", first_number + second_number)
