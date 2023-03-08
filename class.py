155558eaa0eb8afcfd54dfa79a326a1629180088
nderitu_njau <nderitunjau@gmail.com>
2023-03-08 15:42:42 +0300

    Update class.py

--- class.py
+++ class.py
@@ -2,8 +2,9 @@ class Car:
     def __init__():
         self.model = "S600"
         self.color = "Red" # ends up on the object
-        make = "Mercedes" # becomes a local variable in the constructor
+        self.make = "Mercedes" # becomes a local variable in the constructor
 
 car = Car()
 print(car.color) # "Red"
 print(car.make) # would result in an error, `make` does not exist on the object
+#Let us sort that out. by self.make.
