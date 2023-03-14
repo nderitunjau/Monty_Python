27848bfb93ae49fcc405a8216418f4e280b81e70
nderitu_njau <nderitunjau@gmail.com>
2023-03-14 13:57:13 +0300

    Update square.py

--- square.py
+++ square.py
@@ -1,13 +1,19 @@
 class Square:
-      def __init__(self):
-          self.__height = 2
-          self.__width = 2
-    def set_side(self, new_side):
-          self.__height = new_side
-          self.__width = new_side
-
-  square = Square()
-  square.__height = 3 # raises AttributeError
+    def __init__(self, w, h):
+        self.__height = h
+        self.__width = w
   
-#square = Square()
-#square._Square__height = 3 # is allowed
+    def set_side(self, new_side):
+        self.__height = new_side
+        self.__width = new_side
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, new_value):
+        if new_value >= 0:
+            self.__height = new_value
+        else:
+            raise Exception("Value must be larger than 0")
