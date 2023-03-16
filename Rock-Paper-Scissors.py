ffe84d5de9e65b6ac33d0006a6e08b8ce40066f7
nderitu_njau <nderitunjau@gmail.com>
2023-03-16 14:58:05 +0300

    Update Rock-Paper-Scissors.py

--- Rock-Paper-Scissors.py
+++ Rock-Paper-Scissors.py
@@ -3,9 +3,11 @@ class Participant:
         self.name = name
         self.points = 0
         self.choice = ""
+        
     def choose(self):
         self.choice = input("{name}, select rock, paper or scissor: ".format(name= self.name))
         print("{name} selects {choice}".format(name=self.name, choice = self.choice))
+        
     def toNumericalChoice(self):
         switcher = {
             "rock": 0,
