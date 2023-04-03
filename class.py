ec5c6a5f2880595d1f926de0fbb94faf6e16c8d0
nderitu_njau <nderitunjau@gmail.com>
2023-04-03 15:18:56 +0300

    Update class.py

--- class.py
+++ class.py
@@ -6,6 +6,7 @@ class Car:
         self.make = "Mercedes" # becomes a local variable in the constructor
 
 car = Car()
+
 print(car.color) # "Red"
 print(car.make) # would result in an error, `make` does not exist on the object
 #Let us sort that out. by self.make.
