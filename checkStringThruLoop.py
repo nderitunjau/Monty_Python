fe5abb497b737cc49625730fe2077a29485f6d66
nderitu_njau <nderitunjau@gmail.com>
2023-10-16 15:13:16 +0300

    Update checkStringThruLoop.py

--- checkStringThruLoop.py
+++ checkStringThruLoop.py
@@ -2,3 +2,4 @@ mars_temperature = "The highest temperature on Mars is about 30 C"
 for item  in mars_temperature.split():
     if item.isnumeric():
         print(item)
+//Like the .isnumeric() method, .isdecimal() can check for strings that look like decimals.
