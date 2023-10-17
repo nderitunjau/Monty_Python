c590e4455a72847432367ff39e245fcf0e9e483e
nderitu_njau <nderitunjau@gmail.com>
2023-10-17 16:15:45 +0300

    Update listsModify.py

--- listsModify.py
+++ listsModify.py
@@ -1,3 +1,3 @@
 cars=["Mercedes","BMW","Jaguar","Subaru","Supra","Lamborgini","Ferari","Lexus"]
 cars[6]="Ferari Pista"
-print("We have upgraged our Ferari to ",cars[6])
+print("We have upgraged our Ferari to ",cars[6],f"and number of cars is {number_of_cars}")
