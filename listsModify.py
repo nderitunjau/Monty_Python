e468f5d7a11f4b321f8845c50e6ba6bf445b5aeb
nderitu_njau <nderitunjau@gmail.com>
2023-10-17 16:16:14 +0300

    Update listsModify.py

--- listsModify.py
+++ listsModify.py
@@ -1,3 +1,4 @@
 cars=["Mercedes","BMW","Jaguar","Subaru","Supra","Lamborgini","Ferari","Lexus"]
 cars[6]="Ferari Pista"
 print("We have upgraged our Ferari to ",cars[6],f"and number of cars is {number_of_cars}")
+//results is We have upgraged our Ferari to  Ferari Pista and number of cars is 8
