c764375d5c9c93bc57721884b11348177e952939
nderitu_njau <nderitunjau@gmail.com>
2023-10-18 09:26:17 +0300

    Create listswork.py
    Working with lists couple of details in the code

---
+++ listswork.py
@@ -0,0 +1,8 @@
+cars=["Mercedes","BMW","Jaguar","Subaru","Supra","Lamborgini","Ferari","Lexus"]
+cars[6]="Ferari Pista"
+cars.append("Porsche")
+cars.append("Land Rover")
+cars.pop()#removes Landrover in the list
+number_of_cars=len(cars)
+
+print("We have upgraged our Ferari to ",cars[6],f"and number of cars is {number_of_cars}")
