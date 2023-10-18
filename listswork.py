7e618693a156c592283e54a5d96d3637101d26b9
nderitu_njau <nderitunjau@gmail.com>
2023-10-18 10:30:07 +0300

    Update listswork.py

--- listswork.py
+++ listswork.py
@@ -5,4 +5,4 @@ cars.append("Land Rover")
 cars.pop()#removes Landrover in the list
 number_of_cars=len(cars)# shows the length or number of items in my list
 
-print("We have upgraged our Ferari to ",cars[6],f"and number of cars is {number_of_cars}")
+print("We have upgraged our Ferari to ",cars[6],f"and number of cars is {number_of_cars}",f"and the last car is",cars[-1])
