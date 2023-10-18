65acdc1bfc5646053187707657c9ad2f922be5ad
nderitu_njau <nderitunjau@gmail.com>
2023-10-18 09:32:11 +0300

    Update listswork.py

--- listswork.py
+++ listswork.py
@@ -3,6 +3,6 @@ cars[6]="Ferari Pista"
 cars.append("Porsche")
 cars.append("Land Rover")
 cars.pop()#removes Landrover in the list
-number_of_cars=len(cars)
+number_of_cars=len(cars)# shows the length or number of items in my list
 
 print("We have upgraged our Ferari to ",cars[6],f"and number of cars is {number_of_cars}")
